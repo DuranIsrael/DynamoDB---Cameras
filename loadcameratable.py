a705a272ed66a2f79df7ffd09fb844c1829fad23
Duran Israel <98367725+DuranIsrael@users.noreply.github.com>
2022-04-25 21:33:38 -0500

    Update loadcameratable.py

--- loadcameratable.py
+++ loadcameratable.py
@@ -7,15 +7,15 @@ dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
 
 table = dynamodb.Table('Cameras')
 #here it is going to open the json file
-with open("cameradata.json") as json_file:
+with open("cameradata.json") as json_file: #in this line we are using the JSON file that contained our camera information to fill the table with items
     camera = json.load(json_file)
     for cameras in camera:
         brand = cameras['brand']
         model = cameras['model']
 
-        print("Adding cameras:", brand, model)
+        print("Adding cameras:", brand, model) #prints our results
 
-        table.put_item(
+        table.put_item( # this line actually puts the items into the table
            Item={
                'brand': brand,
                'model': model,
