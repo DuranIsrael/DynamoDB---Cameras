c62bd0a3660dc7a298a8276fff52247984ed7aac
Duran Israel <98367725+DuranIsrael@users.noreply.github.com>
2022-04-25 21:34:13 -0500

    Update deleteitem.py

--- deleteitem.py
+++ deleteitem.py
@@ -4,6 +4,6 @@ dynamodb = boto3.resource('dynamodb')
 
 table = dynamodb.Table('Cameras')
 
-response = table.delete_item(Key = {'brand': 'Canon', 'model': 'EOS-R6'})
+response = table.delete_item(Key = {'brand': 'Canon', 'model': 'EOS-R6'}) #here we are deleting a particular item from the table
 
 print(response)
