873df81198e5e92f51d2be9c23c4af1f3c35ada7
Duran Israel <98367725+DuranIsrael@users.noreply.github.com>
2022-04-25 21:38:48 -0500

    Update boto3_step_1.py

--- boto3_step_1.py
+++ boto3_step_1.py
@@ -1,6 +1,9 @@
+#I called this step 1 because we first must give python or our IDE (VSCODE for me) accesss to our AWSaccount in order to spin up and manipulate a table
+#Make sure you have python downloaded on your local machine. From python you must download boto3 before doing this step.
+
 import boto3
 
-# replace the keys below
+# replace the keys below with the desired AWS account you want to use. Make sure this accout had the correct permissions to create and manipulate DynamoDB
 
 dynamodb = boto3.resource(
     'dynamodb',
