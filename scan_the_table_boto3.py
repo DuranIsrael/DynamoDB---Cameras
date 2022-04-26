bc6e50b1f7a672a510bac218b352d129cfc59cb5
Duran Israel <98367725+DuranIsrael@users.noreply.github.com>
2022-04-25 21:31:57 -0500

    Update scan_the_table_boto3.py

--- scan_the_table_boto3.py
+++ scan_the_table_boto3.py
@@ -1,11 +1,13 @@
 import boto3
 from boto3.dynamodb.conditions import Key, Attr
 
-dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('Camera')
+dynamodb = boto3.resource('dynamodb')#the resource or service we are calling is Dynamodb
+table = dynamodb.Table('Camera')# here we are choosing our table
 
 response = table.scan(FilterExpression=Attr('brand').eq('Canon'))
 
-print("The query returned the following items:")
+print("The query returned the following items:") 
 for item in response['Items']:
-    print(item)
+    print(item) # this will print the scanned items
+    
+#in the terminal remeber to run these files using python. IE: python 'filename'
